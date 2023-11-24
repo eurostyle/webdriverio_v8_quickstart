77d4dfea621f449257ee1d77d10eec451547cf28
martynas.andriuskevicius <martynas.andriuskevicius@kilo.health>
2023-11-25 01:37:01 +0200

    new example test added

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -3,13 +3,23 @@ import LoginPage from '../pageobjects/login.page'
 import SecurePage from '../pageobjects/secure.page'
 
 describe('My Login application', () => {
-  it('should login with valid credentials', async () => {
+  
+  before(async () => {
     await LoginPage.open();
-    
+  });
+  
+  it('should login with valid credentials', async function () {
     await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-    await expect(await SecurePage.flashAlert).toBeExisting();
-    await expect(await SecurePage.flashAlert).toHaveText(
-      'You logged into a secure area!');
-  })
+    await expect(SecurePage.flashAlert).toBeDisplayed();
+  });
+  
+  it('Should pass the test only on Chrome', async () => {
+    const browserName = (browser.capabilities as WebdriverIO.Capabilities).browserName as string;
+    if (browserName !== 'chrome') {
+      throw new Error('This test only works with Chrome')
+    }
+    expect(browserName).toEqual('chrome');
+  });
+  
 })
 

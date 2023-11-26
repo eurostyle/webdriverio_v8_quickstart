d1839f7655aae9294e56efe9839817f3870e041e
martynas.andriuskevicius <martynas.andriuskevicius@kilo.health>
2023-11-26 23:22:21 +0200

    added ability to change time frequency to how often the reports will be deleted.

--- utils/allure.cleanup.ts
+++ utils/allure.cleanup.ts
@@ -6,9 +6,9 @@ const reportsPath = 'allure-results';
 /**
  * Deletes old reports from the specified reports directory.
  */
-export function deleteOldReports(): void {
+export function deleteOldReports(minutes: number = 10): void {
   const currentTime: number = Date.now();
-  const tenMinutesInMilliseconds: number = 10 * 60 * 1000;
+  const minutesInMilliseconds: number = minutes * 60 * 1000;
   
   fs.readdir(reportsPath, (err: NodeJS.ErrnoException | null, files: string[]): void => {
     if (err) {
@@ -26,7 +26,7 @@ export function deleteOldReports(): void {
         
         const fileAge: number = currentTime - stats.mtimeMs;
         
-        if (fileAge > tenMinutesInMilliseconds) {
+        if (fileAge > minutesInMilliseconds) {
           fs.unlink(filePath, (err: NodeJS.ErrnoException | null): void => {
             if (err) {
               console.error('Error deleting file:', err);
